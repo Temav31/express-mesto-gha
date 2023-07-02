6d4dd19584b19e47671d9d0341d2cdecc6b58ae5
Temav31 <temav331@mail.ru>
2023-07-03 00:00:16 +0300

    работа готова к проверке

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const react = require("eslint-plugin-import/config/react");
-const userRouter = require('./routes/users');
-
+const router = require("./routes/index");
+// константы
 const app = express();
 app.use(express.json());
 const { PORT = 3000 } = process.env;
-// подключение к базе данных мангус
-mongoose.connect("mongodb://localhost:27017/mestodb", {
-    useNewUrlParser: true,
+// // подключаемся к серверу mongo
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
+  useNewUrlParser: true,
 });
-
-app.use(express.json());
-// app.use(router);
-// app.use((req, res, next) => {
-//     req.user = {
-//         _id: "5d8b8592978f8bd833ca8133", // вставьте сюда _id созданного в предыдущем пункте пользователя
-//     };
-//     next();
-// });
-
-app.use('/users', userRouter);
-
-app.use('/*', (req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: 'Страница не найдена' });
+// роуты
+app.use(router);
+// хардкодим пользователя
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64a1368d2318b016942483e6',
+  };
+  next();
 });
-
+// порт
 app.listen(PORT, () => {
   console.log(`Порт: ${PORT}`);
 });
-//
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,19 +1,110 @@
-// const User = require("../models/user");
-// const {
-//   ERROR_INACCURATE_DATA,
-//   ERROR_NOT_FOUND,
-//   ERROR_INTERNAL_SERVER,
-// } = require("../utils/errors");
-
-// const getCard = (req, res) => {};
-// const createCard = (req, res) => {};
-// const deleteCard = (req, res) => {};
-// const likeCard = (req, res) => {};
-// const deleteLikeCard = (req, res) => {};
-// module.exports = {
-//   getCard,
-//   createCard,
-//   deleteCard,
-//   likeCard,
-//   deleteLikeCard,
-// };
+const Card = require("../models/card");
+const {
+  ERROR_INVALID_DATA,
+  ERROR_NOT_FOUND,
+  ERROR_SERVER,
+} = require("../utils/errors");
+// получение карточек
+const getCard = (req, res) => {
+  console.log("hi");
+  Card.find({})
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) =>
+      res.status(404).send({
+        maessege: "Ошибка сервера",
+        err: err.message,
+        stack: err.stack,
+      })
+    );
+};
+// создать карточку
+const createCard = (req, res) => {
+  Card.create(req.body)
+    .then((cards) => res.status(201).send(cards))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(ERROR_INVALID_DATA).send({
+          message: "Некоректные данные",
+        });
+      } else {
+        res.status(ERROR_SERVER).send({
+          message: "Ошибка сервера",
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+    });
+};
+// поставить лайк карточке
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true }
+  )
+    .orFail(() => new Error("Not Found"))
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res
+          .status(ERROR_INVALID_DATA)
+          .send({ message: "Некоректные данные" });
+      } else if (err.message === "Not Found") {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: "Карточка не найдена" });
+      } else {
+        res.status(ERROR_SERVER).send({ message: "Ошибка сервера" });
+      }
+    });
+};
+// убрать лайк карточке
+const deleteLikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true }
+  )
+    .orFail(() => new Error("Not Found"))
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res
+          .status(ERROR_INVALID_DATA)
+          .send({ message: "Некоректные данные" });
+      } else if (err.message === "Not Found") {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: "Карточка не найдена" });
+      } else {
+        res.status(ERROR_SERVER).send({ message: "Ошибка сервера" });
+      }
+    });
+};
+// удаление карточки
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .orFail(() => new Error("Not found"))
+    .then(() => res.status(201).send({ message: "Карточка удалена" }))
+    .catch((err) => {
+      if (err.name === "Not found") {
+        res.status(ERROR_NOT_FOUND).send({
+          message: "Карточка не найдена",
+        });
+      } else {
+        res.status(ERROR_SERVER).send({
+          message: "Ошибка сервера",
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+    });
+};
+// экспорт контроллеров
+module.exports = {
+  getCard,
+  createCard,
+  deleteLikeCard,
+  deleteCard,
+  likeCard,
+};
