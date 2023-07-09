59e108c2c03d4132ff5d3f7207f8b9dd159d814b
Temav31 <temav331@mail.ru>
2023-07-09 19:34:27 +0300

    3

--- middlwares/error.js
+++ middlwares/error.js
@@ -1,15 +1,4 @@
-// const errorHandler = (err, req, res, next) => {
-//     // проверка ошибок
-//     if (err.statusCode) {
-//         res.status(err.statusCode).send({ message: err.message });
-//     } else {
-//         res.status(500).send({ message: "Ошибка сервера" });
-//     }
-//     console.log(err.statusCode);
-//     next();
-// };
 module.exports = ((err, req, res, next) => {
     res.status(err.statusCode ? err.statusCode : 500).send({ message: err.message });
     next();
 });
-// module.exports = errorHandler;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 //  импорт обработчиков
 const {
     getUsers,
@@ -7,11 +8,17 @@ const {
     UpdateAvatar,
     getCurrentUser
 } = require("../controllers/users");
+// валидация
+const validation = celebrate({
+    params: Joi.object().keys({
+        cardId: Joi.string().length(24).required(),
+    }),
+});
 // обработка путей
-router.patch('/me', UpdateProfile);
-router.patch('/me/avatar', UpdateAvatar);
+router.patch('/me', validation, UpdateProfile);
+router.patch('/me/avatar', validation, UpdateAvatar);
 router.get("/", getUsers);
-router.get("/:id", getUserById);
+router.get("/:id", validation, getUserById);
 router.get('/me', getCurrentUser);
 // экспорт роута
 module.exports = router;
