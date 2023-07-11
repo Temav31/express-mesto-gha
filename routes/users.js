d1d471ce8d0e9b37905dcb7727cb66527e34ff27
Temav31 <temav331@mail.ru>
2023-07-11 11:52:16 +0300

    3

--- routes/users.js
+++ routes/users.js
@@ -1,70 +1,3 @@
-Skip to content
-Temav31
-/
-express-mesto-gha
-
-Type / to search
-Command palette
-Create new...
-Issues
-Pull requests
-You have unread notifications
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-Settings
-Pane width
-Use a value between 17% and 36%
-
-21
-Change width
-Code
-Go to file
-t
-routes content loaded
-.github
-controllers
-cards.js
-users.js
-middlwares
-auth.js
-error.js
-models
-card.js
-user.js
-routes
-cards.js
-index.js
-users.js
-utils
-.editorconfig
-.eslintrc
-.gitignore
-README.md
-app.js
-package-lock.json
-package.json
-Documentation • Share feedback
-Breadcrumbsexpress-mesto-gha/routes
-/users.js
-Latest commit
-Temav31
-Temav31
-15
-5b121dd
- ·
-14 hours ago
-History
-File metadata and controls
-
-Code
-
-Blame
-44 lines (44 loc) · 970 Bytes
 const router = require("express").Router();
 const { celebrate, Joi } = require("celebrate");
 const { pattern } = require("../utils/constants");
@@ -109,4 +42,3 @@ router.get(
 );
 // экспорт роута
 module.exports = router;
-express-mesto-gha/routes/users.js at b29573af4e11b898ce874bf32b41026a987a4efa · Temav31/express-mesto-gha · GitHub
